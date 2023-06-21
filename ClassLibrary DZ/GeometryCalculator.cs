398a92ce5e287c1d1a8330f62660f5c7c90e149e
Admin <Admin@DESKTOP-QQR48TL>
2023-06-21 20:12:58 +0600

    Добавьте файлы проекта.

---
+++ ClassLibrary DZ/GeometryCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_DZ
+{
+    public class GeometryCalculator
+    {
+        public static double CalculateSquareArea(double side)
+        {
+            return side * side;
+        }
+
+        public static double CalculateRectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public static double CalculateTriangleArea(double baseLength, double height)
+        {
+            return 0.5 * baseLength * height;
+        }
+    }
+}
---
+++ GeometryTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary_DZ;
+
+namespace GeometryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareArea = GeometryCalculator.CalculateSquareArea(5);
+            double rectangleArea = GeometryCalculator.CalculateRectangleArea(3, 4);
+            double triangleArea = GeometryCalculator.CalculateTriangleArea(6, 8);
+
+            Console.WriteLine("Площадь квадрата: " + squareArea);
+        }
+    }
+    }
+            
